5239cae5ad1500a919b1b24d5584f7ecc0ed7cf9
Harman Singh <harman28@gmail.com>
2016-06-29 11:09:52 +0530

    Get API for summaries

--- app.rb
+++ app.rb
@@ -9,17 +9,30 @@ get '/' do
 end
 
 get '/updates' do
-  Update.all.to_json
+  team_id = params.with_indifferent_access[:team_id]
+  if team_id.present?
+    prepare_summary(team_id)
+  else
+    status 400
+    "Whoops"
+  end
 end
 
 post '/updates' do
-  p params
-  update_attrs = params.with_indifferent_access.slice(:team_id, :user_id, :user_name, :text)
-  @update = Update.new(update_attrs)
+  create_attrs = params.with_indifferent_access.slice(:team_id, :user_id, :user_name, :text)
+  @update = Update.new(create_attrs)
   if @update.save
     @update.to_json
   else
     status 400
     @update.errors.to_json
   end
+end
+
+def prepare_summary team_id
+  summary = ["Team Updates:"]
+  Update.where(team_id:team_id).find_each do |up|
+    summary << "#{up.user_name}: #{up.text}"
+  end
+  summary.join('\n')
 end
