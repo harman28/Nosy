06e80997b207c03b60966aa03cc10b077ed5051c
Harman Singh <harman28@gmail.com>
2016-06-29 07:39:36 +0530

    Look, there's a DB and everything

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+
+require './models/update' #Update class
+require './config/environments' #database configuration
 
 get '/' do
     "Things appear to be okay."
